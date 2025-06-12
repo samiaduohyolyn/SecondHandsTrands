85ef6fd48b6246687079879dfa9cace33dc0f5b2
samiaduohyolyn <samidora417@gmail.com>
2025-06-12 12:24:34 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+import view.LoginFrame;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 设置Swing外观为系统外观
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 启动登录界面
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LoginFrame().setVisible(true);
+            }
+        });
+    }
+}
